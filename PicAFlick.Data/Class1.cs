f7d75b454121b0dd3be00ce28188ec394c0b0d51
lizbethhahn <lizbethhahn@gmail.com>
2025-03-16 11:48:34 -0400

    added entities, data provider, connection string

--- PicAFlick.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PicAFlick.Data
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ PicAFlick.Data/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using PicAFlick.Domain.Entities;
+
+namespace PicAFlick.Data
+{
+    public class MovieContext : DbContext
+    {   
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<TVShow> TVShows { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source= ECHO;Database=PicAFlickData;Trusted_Connection=True;TrustServerCertificate=True", options => options.MaxBatchSize(100));
+        }
+    }
+}
