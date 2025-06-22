ebd967c72508611abef989f6dcd3fae357ccd23b
lizbethhahn <lizbethhahn@gmail.com>
2025-06-22 12:41:42 -0400

    Wired up search buttons to call Web API for movie and TV data.

--- PicAFlick.SPA/src/app/app.ts
+++ PicAFlick.SPA/src/app/app.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MovieSearchComponent } from './movie-search/movie-search';
+import { TvShowSearchComponent } from './tv-show-search/tv-show-search';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: true,
   styleUrls: ['./app.scss'],
-  imports: [FormsModule, CommonModule, MovieSearchComponent]
+  imports: [FormsModule, CommonModule, MovieSearchComponent, TvShowSearchComponent]
 })
 export class App {
   protected title = 'PicAFlick';
