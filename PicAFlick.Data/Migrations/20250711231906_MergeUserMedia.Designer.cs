08c5c2b3ee3f96809ef12f67ee474a597a0de59e
lizbethhahn <lizbethhahn@gmail.com>
2025-07-11 19:39:07 -0400

    (Migration)-Merge Usermovie/UserTvShow into UserMedia

--- PicAFlick.Data/Migrations/20250711231906_MergeUserMedia.Designer.cs
+++ PicAFlick.Data/Migrations/20250711231906_MergeUserMedia.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PicAFlick.Data;
 
@@ -11,9 +12,11 @@ using PicAFlick.Data;
 namespace PicAFlick.Data.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20250711231906_MergeUserMedia")]
+    partial class MergeUserMedia
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
