b16138435c7a995490cf737cbc93b322eac89a89
lizbethhahn <lizbethhahn@gmail.com>
2025-07-17 19:15:51 -0400

    Defined IWatchlistService and DTOs for watchlist creation/display; small tweak to the WatchlistItem entity

---
+++ PicAFlick.Domain.Core/Interfaces/IWatchlistService.cs
@@ -0,0 +1,18 @@
+﻿using PicAFlick.Domain.Core.Models;
+
+namespace PicAFlick.Domain.Core.Interfaces
+{
+    public interface IWatchlistService
+    {
+        // Create
+        Task<WatchlistDisplayDto> AddItemAsync(WatchlistCreationDto dto);
+
+        // Read
+        Task<List<WatchlistDisplayDto>> GetItemsByUserAsync(string userId);
+        Task<WatchlistDisplayDto?> GetItemsByIdAsync(int id, string userId);
+
+        // Delete
+        Task<bool> RemoveItemAsync(int id, string userId);
+
+    }
+}
---
+++ PicAFlick.Domain.Core/Models/WatchlistCreationDto.cs
@@ -0,0 +1,20 @@
+﻿using PicAFlick.Domain.Core.Enums;
+
+namespace PicAFlick.Domain.Core.Models
+{
+    public class WatchlistCreationDto
+    {
+        // Required Fields
+        public required string Title { get; set; } =  string.Empty;
+        public required MediaType MediaType { get; set; }
+
+        // Tmdb metadata
+        public int? TmdbId { get; set; }
+        public string? PosterPath { get; set; }
+        public int? ReleaseYear { get; set; }
+        public string? Overview {  get; set; }
+
+        // details added by user
+        public string? Notes { get; set; }  
+    }
+}
