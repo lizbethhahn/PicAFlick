b72d02457e0c3e8d83f1c6b5fe7a0306403176d7
lizbethhahn <lizbethhahn@gmail.com>
2025-07-28 00:00:32 -0400

    Added Requests and Responses folders and moved WatchlistCreationDto and WatchlistDisplayDto into them. Updated using statements

---
+++ PicAFlick.WebApi/Requests/WatchlistCreationDto.cs
@@ -0,0 +1,23 @@
+﻿using PicAFlick.Domain.Enums;
+
+namespace PicAFlick.WebApi.Requests
+{
+    public class WatchlistCreationDto
+    {
+        // Assigned by controller
+        public required string UserId { get; set; } = string.Empty;
+
+        // Required Fields
+        public required string Title { get; set; } =  string.Empty;
+        public required MediaType MediaType { get; set; }
+
+        // Tmdb metadata
+        public int TmdbId { get; set; }
+        public string? PosterPath { get; set; }
+        public int? ReleaseYear { get; set; }
+        public string? Overview {  get; set; }
+
+        // details added by user
+        public string? Notes { get; set; }  
+    }
+}
---
+++ PicAFlick.WebApi/Responses/WatchlistDisplayDto.cs
@@ -0,0 +1,25 @@
+﻿using PicAFlick.Domain.Enums;
+
+namespace PicAFlick.WebApi.Responses
+{
+    public class WatchlistDisplayDto
+    {   
+        // Id set by Db
+        public int Id { get; set; }
+
+        // Required fields
+        public required string UserId { get; set; }
+        public required string Title { get; set; }
+        public required MediaType MediaType { get; set; }
+
+        // Tmdb metadata
+        public int? TmdbId { get; set; }
+        public string? PosterPath { get; set; }
+        public int? ReleaseYear { get; set; }
+        public string? Overview { get; set; }
+
+        // details added by user
+        public string? Notes { get; set; }
+        public bool Watched { get; set; }
+    }
+}
