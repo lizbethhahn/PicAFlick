dfff81f81f0b94e0752f0af621c78c33535a1e60
lizbethhahn <lizbethhahn@gmail.com>
2025-12-16 12:47:10 -0500

    Scaffolded a watchlist component

--- PicAFlick.SPA/src/app/app.ts
+++ PicAFlick.SPA/src/app/app.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MovieSearchComponent } from './movie-search/movie-search';
 import { TvShowSearchComponent } from './tv-show-search/tv-show-search';
+import { WatchlistComponent } from './watchlist/watchlist';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: true,
   styleUrls: ['./app.scss'],
-  imports: [FormsModule, CommonModule, MovieSearchComponent, TvShowSearchComponent]
+  imports: [FormsModule, CommonModule, MovieSearchComponent, TvShowSearchComponent, WatchlistComponent]
 })
 export class AppComponent {
   protected title = 'PicAFlick';
