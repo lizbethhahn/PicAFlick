6b536dad06f4f7f2ea2900f365e521aa75ed83ca
lizbethhahn <lizbethhahn@gmail.com>
2026-03-07 17:31:40 -0500

    Add TMDb client interface implementation and DTOs for search response

---
+++ CapstoneChatbot/CapstoneChatbot.Tmdb/Clients/TmdbClient.cs
@@ -0,0 +1,15 @@
+﻿using CapstoneChatbot.Tmdb.Enums;
+
+namespace CapstoneChatbot.Tmdb.Clients
+{
+    public class TmdbClient : ITmdbClient
+    {
+        public Task<IReadOnlyList<Models.TmdbSearchResult>> SearchAsync(
+            string query,
+            MediaType mediaType,
+            CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
