c48eec83357846c7522a7d2e7470732dce6d81bc
lizbethhahn <lizbethhahn@gmail.com>
2026-03-07 15:00:47 -0500

    Add Overview and PosterPath to WatchlistItem and configure MediaType enum string conversion

--- CapstoneChatbot/CapstoneChatbot.App/Data/CapstoneDbContext.cs
+++ CapstoneChatbot/CapstoneChatbot.App/Data/CapstoneDbContext.cs
@@ -14,8 +14,11 @@ public class CapstoneDbContext : DbContext
         modelBuilder.Entity<WatchlistItem>(e =>
         {
             e.Property(x => x.Title).IsRequired().HasMaxLength(300);
-            e.Property(x => x.MediaType).IsRequired().HasMaxLength(20);
-
+            e.Property(x => x.MediaType)
+             .HasConversion<string>()
+             .IsRequired();
+            e.Property(x => x.Overview).HasMaxLength(2000);
+            e.Property(x => x.PosterPath).HasMaxLength(500);
             // Rating like 1.0, 1.5, 2.0 ... 5.0
             e.Property(x => x.Rating).HasPrecision(2, 1);
         });
